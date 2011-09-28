909eab7f9f3bdffdf743607fa3eade99f65ea983
Andy Richardson <rishson@gmail.com>
2011-09-28 23:01:37 +0100

    Moved robot test dir

--- test/doh/tests.js
+++ test/doh/tests.js
@@ -1,3 +1,6 @@
 dojo.provide('test.doh.Tests');
 
 dojo.require('test.doh.control.TestController');
+dojo.require('test.doh.control.TestMockTransport');
+dojo.require('test.doh.control.TestRequest');
+dojo.require('test.doh.control.TestServiceRequest');
