674797f44153b1d6bdf6a300f4c3a6b171fdb967
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2011-12-12 10:28:35 -0500

    AMD: convert build/EnterpriseDeps, control/Controller, view/AppContainer

--- src/js/rishson/build/EnterpriseDeps.js
+++ src/js/rishson/build/EnterpriseDeps.js
@@ -3,26 +3,26 @@ don't put any code other than require/provide statements in it. When we
 make a build, this will be replaced by a single minified copy of all
 the modules listed below, as well as their dependencies, all in the
 right order:*/
-dojo.provide("rishson.enterprise.build.EnterpriseDeps");
-
 
 /*Dojo dependencies - things that Enterprise needs from Dojo */
-dojo.require("dijit.dijit");
-/* pulls in:
-dojo.require("dijit._base");
-dojo.require("dojo.parser");
-dojo.require("dijit._Widget");
-dojo.require("dijit._Templated");
-dojo.require("dijit._Container");
-dojo.require("dijit.layout._LayoutWidget");
-dojo.require("dijit.form._FormWidget");
-*/
-
-dojo.require("dojo.i18n");
-
-dojo.require("dojox.analytics.plugins.consoleMessages");
-
-/*Enterprise dependencies - package up all of Enterprise into 1 file*/
-dojo.require("rishson.enterprise.control.Controller");  //pulls in util.ObjectValidator
-dojo.require("rishson.enterprise.control.ServiceRequest");  //pulls in Request
-dojo.require("rishson.enterprise.XhrTransport");    //pulls in Transport
+define([
+	"dijit/dijit",
+	/* pulls in:
+		"dijit/_base",
+		"dojo/parser",
+		"dijit/_Widget",
+		"dijit/_TemplatedMixin",
+		"dijit/_Container",
+		"dijit/layout/_LayoutWidget",
+		"dijit/form/_FormWidget",
+		"dijit/form/_FormValueWidget",
+	*/
+	"dijit/_WidgetsInTemplateMixin", // for templated widgets with child widgets
+	"dojo/i18n",
+	"dojox/analytics/plugins/consoleMessages",
+	
+	/*Enterprise dependencies - package up all of Enterprise into 1 file*/
+	"rishson/control/Controller", //pulls in util/ObjectValidator
+	"rishson/control/ServiceRequest",  //pulls in Request
+	"rishson/enterprise/XhrTransport" //pulls in Transport
+]);
--- src/js/rishson/view/AppContainer.js
+++ src/js/rishson/view/AppContainer.js
@@ -1,126 +1,132 @@
-dojo.provide('rishson.enterprise.view.AppContainer');
-
-dojo.require('rishson.enterprise.widget._Widget');
-dojo.require('dijit.layout._LayoutWidget');
-dojo.require('dijit._Container');
-dojo.require('dijit._Templated');
-dojo.require('rishson.enterprise.util.ObjectValidator');
-
-//template widgets
-dojo.require('dijit.layout.BorderContainer');
-dojo.require('dijit.layout.ContentPane');
-
-/**
- * @class
- * @name rishson.enterprise.view.AppContainer
- * @description This is the topmost widget that is designed to contain your application.
- */
-dojo.declare('rishson.enterprise.view.AppContainer', [rishson.enterprise.widget._Widget, dijit.layout._LayoutWidget,
-    dijit._Templated, dijit._Container], {
-
-    templateString : dojo.cache("rishson.enterprise.view", "appContainer/AppContainer.html"),
-    widgetsInTemplate : true,
-
+define([
+    "rishson/widget/_Widget",
+    "dijit/layout/_LayoutWidget",
+    "dijit/_Container",
+    "dijit/_TemplatedMixin",
+    "dijit/_WidgetsInTemplateMixin",
+    "dojo/text!rishson/view/appContainer/AppContainer.html",
+    "rishson/util/ObjectValidator",
+    "dojo/_base/declare", // declare + safeMixin
+    "dojo/_base/lang", // hitch
+    "dojo/dom-class", // add, remove
+    "dojo/topic", // publish/subscribe
+    //template widgets
+    "dijit/layout/BorderContainer",
+    "dijit/layout/ContentPane"
+], function(_Widget, _LayoutWidget, _Container, _TemplatedMixin, _WidgetsInTemplateMixin,
+        template, ObjectValidator, declare, lang, domClass, topic){
+    
     /**
-     * @field
-     * @name rishson.enterprise.view.AppContainer.username
-     * @type {String}
-     * @description the Username of the currently logged in user to display in the header
+     * @class
+     * @name rishson.view.AppContainer
+     * @description This is the topmost widget that is designed to contain your application.
      */
-    username : '',
-
-    /**
-     * @field
-     * @name rishson.enterprise.view.AppContainer.footerText
-     * @type {String}
-     * @description the text to display in the footer
-     */
-    footerText : '',
-
-    /**
-     * @constructor
-     * @param {Object} params contains the username and footerText
-     */
-    constructor : function(params) {
-        var criteria = [{paramName : 'username', paramType : 'string'}, {paramName : 'footerText', paramType : 'string'}];
-        var validator = new rishson.enterprise.util.ObjectValidator(criteria);
-        var unwrappedParams = {username: params.username, footerText: params.footerText};
-        if (validator.validate(unwrappedParams)) {
-            dojo.safeMixin(this, unwrappedParams);
+    declare('rishson.view.AppContainer', [_Widget, _LayoutWidget,
+            _TemplatedMixin, _WidgetsInTemplateMixin, _Container], {
+    
+        templateString : template,
+    
+        /**
+         * @field
+         * @name rishson.view.AppContainer.username
+         * @type {String}
+         * @description the Username of the currently logged in user to display in the header
+         */
+        username : '',
+    
+        /**
+         * @field
+         * @name rishson.view.AppContainer.footerText
+         * @type {String}
+         * @description the text to display in the footer
+         */
+        footerText : '',
+    
+        /**
+         * @constructor
+         * @param {Object} params contains the username and footerText
+         */
+        constructor : function(params) {
+            var criteria = [{paramName : 'username', paramType : 'string'}, {paramName : 'footerText', paramType : 'string'}];
+            var validator = new ObjectValidator(criteria);
+            var unwrappedParams = {username: params.username, footerText: params.footerText};
+            if (validator.validate(unwrappedParams)) {
+                declare.safeMixin(this, unwrappedParams);
+            }
+            else {
+                validator.logErrorToConsole(params, 'Invalid params passed to the AppContainer.');
+                throw ('Invalid params passed to the AppContainer.');
+            }
+        },
+    
+        /**
+         * @function
+         * @name rishson.view.AppContainer
+         * @override rishson.widget._Widget
+         */
+        postCreate : function () {
+            //additions to the subList
+            topic.subscribe(this.subList.WIDGET_INITIALISED, lang.hitch(this, "_handleWidgetInitialisation"));
+    
+            //additions to our pubList
+            this.pubList.LOGOUT = this._topicNamespace + '/user/logout';
+    
+            this.connect(this.dapLogout, 'onclick', this, this._handleLogout);
+            this.connect(this.dapLogout, 'onmouseenter', this, function() {domClass.add(this.dapLogout, 'mouseEnter')});
+            this.connect(this.dapLogout, 'onmouseleave', this, function() {domClass.remove(this.dapLogout, 'mouseEnter')});
+            this.connect(this.dapUsername, 'onmouseenter', this, function() {domClass.add(this.dapUsername, 'mouseEnter headerButton')});
+            this.connect(this.dapUsername, 'onmouseleave', this, function() {domClass.remove(this.dapUsername, 'mouseEnter headerButton')});
+    
+            this.inherited(arguments);  //rishson.widget._Widget
+            this._i18n();
+        },
+    
+        /**
+         * @function
+         * @name rishson.view.AppContainer
+         * @override dijit._Container
+         */
+        startup : function () {
+            this.mainContainer.startup();
+            this.inherited(arguments);
+        },
+    
+        /**
+         * @function
+         * @name rishson.view.AppContainer
+         * @override dijit.layout._LayoutWidget
+         */
+        resize : function() {
+            this.mainContainer.resize();
+            this.inherited(arguments);
+        },
+    
+        _i18n : function() {
+            this.dapWelcomeText.innerHTML = this._nlsStrings.WELCOME;
+            this.dapUsername.innerHTML = this.username + '.';
+            this.dapLogout.innerHTML = this._nlsStrings.LOGOUT;
+            this.dapFooterText.innerHTML = this.footerText;
+        },
+    
+        /**
+         * @function
+         * @private
+         * @param initialisedWidgetId {Object} the string id of the widget that has just been initialised.
+         * @description Handle a widget becoming initialised.
+         */
+        _handleWidgetInitialisation : function (initialisedWidgetId) {
+    
+        },
+    
+        /**
+         * @function
+         * @private
+         * @description Log the session out. Send a request to the server to logout.
+         * The server should respond with a re-direct and a server side session invalidation.
+         */
+        _handleLogout : function () {
+            topic.publish(this.pubList.LOGOUT);
         }
-        else {
-            validator.logErrorToConsole(params, 'Invalid params passed to the AppContainer.');
-            throw ('Invalid params passed to the AppContainer.');
-        }
-    },
-
-    /**
-     * @function
-     * @name rishson.enterprise.view.AppContainer
-     * @override rishson.enterprise.widget._Widget
-     */
-    postCreate : function () {
-        //additions to the subList
-        dojo.subscribe(this.subList.WIDGET_INITIALISED, this, "_handleWidgetInitialisation");
-
-        //additions to our pubList
-        this.pubList.LOGOUT = this._topicNamespace + '/user/logout';
-
-        this.connect(this.dapLogout, 'onclick', this, this._handleLogout);
-        this.connect(this.dapLogout, 'onmouseenter', this, function() {dojo.addClass(this.dapLogout, 'mouseEnter')});
-        this.connect(this.dapLogout, 'onmouseleave', this, function() {dojo.removeClass(this.dapLogout, 'mouseEnter')});
-        this.connect(this.dapUsername, 'onmouseenter', this, function() {dojo.addClass(this.dapUsername, 'mouseEnter headerButton')});
-        this.connect(this.dapUsername, 'onmouseleave', this, function() {dojo.removeClass(this.dapUsername, 'mouseEnter headerButton')});
-
-        this.inherited(arguments);  //rishson.enterprise.widget._Widget
-        this._i18n();
-    },
-
-    /**
-     * @function
-     * @name rishson.enterprise.view.AppContainer
-     * @override dijit._Container
-     */
-    startup : function () {
-        this.mainContainer.startup();
-        this.inherited(arguments);
-    },
-
-    /**
-     * @function
-     * @name rishson.enterprise.view.AppContainer
-     * @override dijit.layout._LayoutWidget
-     */
-    resize : function() {
-        this.mainContainer.resize();
-        this.inherited(arguments);
-    },
-
-    _i18n : function() {
-        this.dapWelcomeText.innerHTML = this._nlsStrings.WELCOME;
-        this.dapUsername.innerHTML = this.username + '.';
-        this.dapLogout.innerHTML = this._nlsStrings.LOGOUT;
-        this.dapFooterText.innerHTML = this.footerText;
-    },
-
-    /**
-     * @function
-     * @private
-     * @param initialisedWidgetId {Object} the string id of the widget that has just been initialised.
-     * @description Handle a widget becoming initialised.
-     */
-    _handleWidgetInitialisation : function (initialisedWidgetId) {
-
-    },
-
-    /**
-     * @function
-     * @private
-     * @description Log the session out. Send a request to the server to logout.
-     * The server should respond with a re-direct and a server side session invalidation.
-     */
-    _handleLogout : function () {
-        dojo.publish(this.pubList.LOGOUT);
-    }
-
+    
+    });
 });
