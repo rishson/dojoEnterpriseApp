019c80c59a201057f2e95d48c7bbae9ecfdd4d2d
Bryan Forbes <bryan@sitepen.com>
2011-12-12 18:40:51 -0600

    SCRIPTS: Only use commands available on both OS X and Linux and add more transforms for setup.sh.

--- scripts/createApplication.sh
+++ scripts/createApplication.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-SCRIPT_PATH="$(readlink -f $0)"
-SCRIPT_NAME="$(basename $SCRIPT_PATH)"
-SCRIPT_DIR="$(dirname $SCRIPT_PATH)"
+SCRIPT_DIR="$(cd $(dirname -- $0) && pwd -P)"
+SCRIPT_NAME="$(basename -- $0)"
 
-LIB_PATH="$(dirname $SCRIPT_DIR)"
+LIB_PATH="$(dirname -- $SCRIPT_DIR)"
 PROJECT_NAME="$1"
 
 function usage {
@@ -20,15 +19,17 @@ fi
 
 TARGET_DIR="$2"
 
-if [ -z $TARGET_DIR ]; then
-	TARGET_DIR=$(pwd)
-elif [ ! -d $TARGET_DIR ]; then
+if [ -z "$2" ]; then
+	TARGET_DIR=$(pwd -P)
+elif [ ! -d "$2" ]; then
 	echo "TARGET_DIRECTORY must exist"
 	usage
 	exit 1
+else
+	TARGET_DIR="$(cd \"$2\" && pwd -P)"
 fi
 
-cd "$TARGET_DIRECTORY"
+cd "$TARGET_DIR"
 mkdir "$PROJECT_NAME"
 cd "$PROJECT_NAME"
 mkdir "scripts"
@@ -37,4 +38,5 @@ cp -r "$LIB_PATH/src" .
 cp -r "$SCRIPT_DIR/build.sh" "$SCRIPT_DIR/setup.sh" scripts
 
 # Translate scripts to app-specific scripts
-sed -i 's/\(PROJECT_DIR="\)$1\("\)/\1$(dirname $SCRIPT_DIR)\2/' "scripts/setup.sh"
+sed -i 's/^\(PROJECT_DIR="\).*\("\)$/\1$(dirname $SCRIPT_DIR)\2/' "scripts/setup.sh"
+perl -i -pe 'BEGIN{undef $/;} s/if \[ -z "\$1" \]; then.*?fi\n\n//s' "scripts/setup.sh"
