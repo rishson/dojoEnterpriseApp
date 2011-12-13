72e27b711eaab6a83db118b3b1d81c1b104214ff
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2011-12-13 09:46:32 -0500

    Add _WidgetInWidgetMixin test page

--- src/js/rishson/tests/doh/Tests.js
+++ src/js/rishson/tests/doh/Tests.js
@@ -8,6 +8,7 @@ define([
     "test/doh/control/TestRestRequest",
     "test/doh/control/TestServiceRequest"
 ], function(doh, require){
+    doh.registerUrl("WidgetInWidgetMixin tests", require.toUrl("./doh/widget/TestWidgetInWidgetMixin.html"), 999999);
     doh.registerUrl("AppContainer tests", require.toUrl("./doh/view/TestAppContainer.html"), 999999);
     doh.registerUrl("_ApplicationWidget tests", require.toUrl("./doh/widget/TestApplicationWidget.html"), 999999);
 });
