0a0e0ce83fba432df1a108925933a2a1e9a8c2f7
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2011-12-22 14:01:00 -0500

    Fix mistake in Spanish i18n resource for example widget

--- src/js/rishson/widget/example/nls/es/Wishlist.js
+++ src/js/rishson/widget/example/nls/es/Wishlist.js
@@ -1,13 +1,11 @@
 define({
-    root: {
-        SortBy: "Ordenar por:",
-        Descending: "Descendente",
-        Name: "Nombre",
-        DateAdded: "Fecha de entrada",
-        Price: "Precio",
-        Actions: "Acciones",
-        Add: "Añadir",
-        Remove: "Eliminar",
-        Save: "Guardar"
-    }
+    SortBy: "Ordenar por:",
+    Descending: "Descendente",
+    Name: "Nombre",
+    DateAdded: "Fecha de entrada",
+    Price: "Precio",
+    Actions: "Acciones",
+    Add: "Añadir",
+    Remove: "Eliminar",
+    Save: "Guardar"
 });
