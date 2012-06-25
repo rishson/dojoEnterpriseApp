6ee704d3726c51606076c03b58e172c3f2649ac5
francis-saul <francis.saul@thomsonreuters.com>
2012-06-25 09:47:45 +0100

    Removed reference to loadingGroups from _Controller mixin.

--- src/js/rishson/control/_Controller.js
+++ src/js/rishson/control/_Controller.js
@@ -140,42 +140,6 @@ define([
 		},
 
 		/**
-		 * @name rishson.control._Controller.loadGroup
-		 * @description Take a loadGroup, loop over each widget and create and add that widget to the controller.
-		 * @param {Object} scope the scope of the current controller. Assignment needs to occur before initialisation
-		 * @param {Array} group
-		 */
-		loadGroup: function (scope, group) {
-			var i, l;
-			for (i = 0, l = group.toLoad.length; i < l; i += 1) {
-				this.addAndInitialise(scope, group.toLoad[i], group, i);
-			}
-		},
-
-		/**
-		 *
-		 * @function
-		 * @name rishson.control._Controller.addAndInitialise
-		 * @description adopted widgets from controllers should fire their initialised event after they have been assigned at
-		 * to the controller assignee
-		 * @param {Object} scope the scope of the current controller. Assignment needs to occur before initialisation
-		 * @param {{name: string, widget: string, loadingGroup: Array, props: Object, node: string|Object}} args The
-		 * initialisation properties for the widget to be included on the page.
-		 * @param {Array} group
-		 * @param {number} i
-		 */
-		addAndInitialise: function (scope, args, group, i) {
-			var obj;
-
-			if (scope && args.name && args.widget) {
-				obj = scope.views[args.name] = this.adopt(args.widget, args.props, args.node);
-				if (group && typeof i !== 'undefined') {
-					group.toLoad[i] = obj;
-				}
-			}
-		},
-
-		/**
 		 * @function
 		 * @name rishson.control._Controller.adopt
 		 * @description widgets injected into this class will be examined to autowire its publish and subscribes.<p>
@@ -199,35 +163,6 @@ define([
 
 			this._autowirePubs(child);
 			return child;
-		},
-
-		/**
-		 * @function
-		 * @name rishson.control._Controller.checkLoadingGroups
-		 * @description Compare the id of the widget which is just initialised with the loading groups array, and move
-		 * from toLoad to loaded if found. Attach the widgets to the layout if all of them have been loaded.
-		 * TODO: smarter 'loadingGroups' logic needs to be created so that there can be more than one loadingGroup array
-		 * and faster indexing.
-		 * @param {string} id
-		 */
-		checkLoadingGroups: function (id) {
-			var i,
-				l,
-				match = false;
-
-			//if all widgets that I care about have been initialised then I should attach them and publish my initialise to my parent
-			for (i = 0, l = this.loadingGroups.toLoad.length; i < l; i += 1) {
-				if (this.loadingGroups.toLoad[i] && this.loadingGroups.toLoad[i]._id === id) {
-					this.loadingGroups.loaded[i] = this.loadingGroups.toLoad[i];
-					delete this.loadingGroups.toLoad[i];
-					match = true;
-					break;
-				}
-			}
-
-			if (match && this.loadingGroups.loaded.length === this.loadingGroups.toLoad.length) {
-				this.attachWidgetsToLayout(this.loadingGroups.loaded);
-			}
 		},
 
 		/**
