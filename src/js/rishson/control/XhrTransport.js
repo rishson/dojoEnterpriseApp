efd39888e7e01638f8d5cbb10fc31a4b00a341c8
francis-saul <francis@mummybot.com>
2012-07-13 09:46:16 +0100

    Added lang.hitch around XhrTransport error to correct 'this' scope.

--- src/js/rishson/control/XhrTransport.js
+++ src/js/rishson/control/XhrTransport.js
@@ -92,13 +92,13 @@ define([
 						ioArgs);
 					this.handleResponseFunc(request, wrappedResponse);
 				});
-				xhrParams.error = function (response, ioArgs) {
+				xhrParams.error = lang.hitch(this, function (response, ioArgs) {
 					var wrappedResponse = new Response(response,
 						request.type === 'rest',
 						ioArgs);
 					//do we think that this 'error' is a valid response, e.g. a 400 REST response?
 					if (wrappedResponse.mappedStatusCodes.indexOf(ioArgs.xhr.status) > -1) {
-						this.handleResponseFunc(request, wrappedResponse);
+						this.handleErrorFunc(request, wrappedResponse);
 					} else {
 						/* Unhandled error - something went wrong in the XHR request/response that we dont cope with.
 						 * This can happen for a timeout or an unhandled status code.
@@ -109,7 +109,7 @@ define([
 						this.handleErrorFunc(request, response);
 						//you could do further processing such as put the transport in a retry or quiescent state
 					}
-				};
+				});
 				if (request.type === 'rest') {
 					xhrFunction = xhr[request.verb]; // get, put, post, or delete
 					if (request.verb === 'put') {
