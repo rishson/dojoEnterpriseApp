0a2323c48dd76683b74e6f31cad72c564f4c6d3b
stephen-bradshaw <stephen.bradshaw@thomsonreuters.com>
2012-08-13 10:17:45 +0100

    Remove console.log used for testing

--- src/js/rishson/control/_Controller.js
+++ src/js/rishson/control/_Controller.js
@@ -87,7 +87,6 @@ define([
 
 					// If a handle was found then remove the subscription
 					if (handle) {
-						console.log("Unautowiring: " + pubHandleName);
 						this.unsubscribe(handle);
 						delete this.subListHandles[pubHandleName];
 					}
